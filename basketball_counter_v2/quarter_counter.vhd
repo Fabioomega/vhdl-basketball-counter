92a726a4e2ccf2225d185fc1ea971286336c2e60
Ian <ian@extradigital.com.br>
2024-11-28 19:37:11 -0300

    se o fábio prestasseatenção no projeto, já teria percebido a 4 dias que os arquivos estavam faltando

---
+++ basketball_counter_v2/quarter_counter.vhd
@@ -0,0 +1,81 @@
+----------------------------------------------------------------------------------
+-- Company: 
+-- Engineer: 
+-- 
+-- Create Date:    19:02:14 11/26/2024 
+-- Design Name: 
+-- Module Name:    quarter_counter - Behavioral 
+-- Project Name: 
+-- Target Devices: 
+-- Tool versions: 
+-- Description: 
+--
+-- Dependencies: 
+--
+-- Revision: 
+-- Revision 0.01 - File Created
+-- Additional Comments: 
+--
+----------------------------------------------------------------------------------
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use work.constants.ALL;
+
+entity quarter_counter is
+    port(
+        clk: in std_logic;
+        enable: in std_logic;
+        state: in STATE;
+        reset: in std_logic;
+        quarter: out integer range 0 to 4;
+        valor_carregado: in integer range 0 to 4;
+        quarter_enable: out std_logic
+    );
+end quarter_counter;
+
+architecture Behavioral of quarter_counter is
+    signal contador_interno: integer range 0 to 4;
+begin
+    process(clk, reset)
+	 begin
+        if reset = '1' then
+            contador_interno <= 4;
+            quarter_enable <= '0';
+        elsif clk'event and clk = '1' then
+            case state is
+                when REP =>
+                    -- Reseta o contador para 4
+                    contador_interno <= 4;
+                    quarter_enable <= '0';
+
+                when CONTA =>
+                    -- Estado de contagem decrementa se habilitado e maior que 0
+                    if enable = '1' then
+                        if contador_interno > 0 then
+                            contador_interno <= contador_interno-1;
+                            quarter_enable <= '0';
+                        else
+                            quarter_enable <= '1';
+                        end if;
+                    end if;
+                
+                when LOAD =>
+                    -- Estado de carga (carregam valor externo)
+                    contador_interno <= valor_carregado;
+                    quarter_enable <= '0';
+                
+                when PARADO =>
+                    -- Estado parado (mantém o valor atual)
+                    quarter_enable <= '0';
+                
+                when others =>
+                    -- Estado de segurança
+                    contador_interno <= contador_interno;
+                    quarter_enable <= '0';
+            end case;
+        end if;
+    end process;
+                        
+    quarter <= contador_interno;            
+end Behavioral;
+
---
+++ basketball_counter_v2/tb_quarter_counter.vhd
@@ -0,0 +1,113 @@
+--------------------------------------------------------------------------------
+-- Company: 
+-- Engineer:
+--
+-- Create Date:   19:27:56 11/26/2024
+-- Design Name:   
+-- Module Name:   /home/ianan/HDL/vhdl-basketball-counter/basketball_counter_v2/tb_quarter_counter.vhd
+-- Project Name:  basketball_counter_v2
+-- Target Device:  
+-- Tool versions:  
+-- Description:   
+-- 
+-- VHDL Test Bench Created by ISE for module: quarter_counter
+-- 
+-- Dependencies:
+-- 
+-- Revision:
+-- Revision 0.01 - File Created
+-- Additional Comments:
+--
+-- Notes: 
+-- This testbench has been automatically generated using types std_logic and
+-- std_logic_vector for the ports of the unit under test.  Xilinx recommends
+-- that these types always be used for the top-level I/O of a design in order
+-- to guarantee that the testbench will bind correctly to the post-implementation 
+-- simulation model.
+--------------------------------------------------------------------------------
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE work.constants.ALL;
+ 
+-- Uncomment the following library declaration if using
+-- arithmetic functions with Signed or Unsigned values
+--USE ieee.numeric_std.ALL;
+ 
+ENTITY tb_quarter_counter IS
+END tb_quarter_counter;
+ 
+ARCHITECTURE behavior OF tb_quarter_counter IS 
+ 
+
+   --Inputs
+   signal clk : std_logic := '0';
+   signal enable : std_logic := '0';
+   signal state : STATE := REP;
+   signal reset : std_logic := '0';
+   signal valor_carregado : integer := 3;
+
+ 	--Outputs
+   signal quarter : integer;
+   signal quarter_enable : std_logic;
+
+   -- Clock period definitions
+   constant clk_period : time := 10 ns;
+ 
+BEGIN
+ 
+	-- Instantiate the Unit Under Test (UUT)
+   uut: entity work.quarter_counter PORT MAP (
+          clk => clk,
+          enable => enable,
+          state => state,
+          reset => reset,
+          quarter => quarter,
+          valor_carregado => valor_carregado,
+          quarter_enable => quarter_enable
+        );
+
+   -- Clock process definitions
+   clk_process :process
+   begin
+		clk <= '0';
+		wait for clk_period/2;
+		clk <= '1';
+		wait for clk_period/2;
+   end process;
+ 
+
+   -- Stimulus process
+   stim_proc: process
+   begin		
+      -- Reset
+      reset <= '1';
+      wait for 20 ns;
+      reset <= '0';
+
+      -- Testar REP (reset do contador)
+      state <= REP;
+      wait for clk_period * 5;
+
+      -- Testa LOAD (carregar valor)
+      state <= LOAD;
+      valor_carregado <= 3;
+      wait for clk_period * 5;
+
+      -- Testa CONTA (contagem decrescente)
+      state <= CONTA;
+      enable <= '1';
+      wait for clk_period * 5;
+
+      -- Teste PARADO (pausa)
+      state <= PARADO;
+      enable <= '0';
+      wait for clk_period * 5;
+
+      -- Retorna estado REP (renício)
+      state <= REP;
+      wait for clk_period * 5;
+
+      wait;
+   end process;
+
+END;
