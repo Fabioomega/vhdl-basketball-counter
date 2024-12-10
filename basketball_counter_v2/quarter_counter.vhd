8b1509072c18614e3228bb5db452ac7d50d91e2e
Ian <ian@extradigital.com.br>
2024-12-10 13:45:17 -0300

    Alterando o contador para crescente

--- basketball_counter_v2/quarter_counter.vhd
+++ basketball_counter_v2/quarter_counter.vhd
@@ -34,25 +34,25 @@ entity quarter_counter is
 end quarter_counter;
 
 architecture Behavioral of quarter_counter is
-    signal contador_interno: integer range 0 to 4;
+    signal contador_interno: integer range 1 to 4;
 begin
     process(clk, reset)
 	 begin
         if reset = '1' then
-            contador_interno <= 4;
+            contador_interno <= 1;
             quarter_enable <= '0';
         elsif clk'event and clk = '1' then
             case state is
                 when REP =>
                     -- Reseta o contador para 4
-                    contador_interno <= 4;
+                    contador_interno <= 1;
                     quarter_enable <= '0';
 
                 when CONTA =>
                     -- Estado de contagem decrementa se habilitado e maior que 0
                     if enable = '1' then
-                        if contador_interno > 0 then
-                            contador_interno <= contador_interno-1;
+                        if contador_interno > 4 then
+                            contador_interno <= contador_interno+1;
                             quarter_enable <= '0';
                         else
                             quarter_enable <= '1';
